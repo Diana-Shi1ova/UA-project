42c5fb750b2b98bd826bba13c31fa15852e41712
Diana-Sh1lova <shilovada@gmail.com>
2025-06-06 16:43:28 +0200

    Finalizando

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT;
 //const {errorHandler} = require('./middleware/errorMiddleware');
@@ -19,6 +20,11 @@ app.use('/api/formats', require('./routes/formatRoutes'));
 app.use('/api/histories', require('./routes/historyRoutes'));
 app.use('/api/tags', require('./routes/tagRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
+
+app.use(cors({
+  origin: 'https://ua-project.vercel.app/',
+  credentials: true
+}));
 
 //app.use(errorHandler);
 
